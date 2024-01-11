1e189b25ddb900dfa678ae64e86473f574bcf202
Shlammy <shlumhum@outlook.com>
2024-01-10 23:25:27 -0500

    Initial config

---
+++ blogDweller/clientsServers/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+
+// register view enginge
+app.set('view engine', 'ejs');
+
+// listen for requests
+app.listen(3000);
+
+// home
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+// about
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' })
+});
+
+// create page
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create Blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+});
